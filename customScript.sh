29c9e98d1f1fc8157d4b0e3176b652924ea62654
Omar Al Jubairi <omar.geberi@gmail.com>
2023-01-23 12:22:03 -0500

    update script

--- customScript.sh
+++ customScript.sh
@@ -2,18 +2,13 @@
 
 #install docker
 yum install -y yum-utils
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     
 yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 systemctl start docker
-
 systemctl enable docker
-chmod +x /usr/local/bin/docker-machine
-export PATH=$PATH:/usr/local/bin/
-systemctl restart docker
+
 
 #install git
 yum update -y
