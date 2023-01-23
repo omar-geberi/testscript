a078ab224fc1c8a011fcb1cf1cfd35af28e6efc9
Omar Al Jubairi <omar.geberi@gmail.com>
2023-01-23 11:47:17 -0500

    Change repo for docker installation

--- customScript.sh
+++ customScript.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #install docker
-wget -qO- "https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
-rpm --import "https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
 yum install -y yum-utils
-yum-config-manager --add-repo https://packages.docker.com/$dockerVer/yum/repo/main/centos/7
-yum install -y docker-engine
-gpasswd -a $userName docker
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 systemctl start docker
+
 systemctl enable docker
 chmod +x /usr/local/bin/docker-machine
 export PATH=$PATH:/usr/local/bin/
